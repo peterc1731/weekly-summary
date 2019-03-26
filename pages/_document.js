4a80767eacbbc1ff8945d98fc40f556c2abaf610
E1200533 <peter.carpenter@santander.co.uk>
2019-03-26 14:59:09 +0000

    added meta tags for android and iphone support

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,8 @@ export default class MyDocument extends Document {
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#EC0000" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="#EC0000" />
           </Head>
           <GlobalStyles />
           <App {...props} />
